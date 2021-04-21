946f43befd087fe15287c07c41c1d95ea7f84b67
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-04-21 14:38:09 +0300

    feat: 01_6kyu_characters_in_your_string.js solution add

---
+++ 01_6kyu_Count_characters_in_your_string.js
@@ -0,0 +1,21 @@
+// TASK DESCRIPTION:
+
+// The main idea is to count all the occurring characters in a string.
+// If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+// What if the string is empty? Then the result should be empty object literal, {}.
+
+function count(string) {
+  let duplicateObj = {};
+
+  for (let char of string) {
+    console.log(char);
+    if (duplicateObj[char] === undefined) {
+      duplicateObj[char] = 1;
+    } else {
+      duplicateObj[char]++;
+      console.log(`duplicates`);
+    }
+  }
+  return duplicateObj;
+}
